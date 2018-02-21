10ed5cdcca086e6b14c9bc177e4bb7d253ba3ee7
Darius Mihai <dariusmihaim@gmail.com>
2018-02-21 18:57:01 +0200

    Add build randisk platform option with default value
    Signed-off-by: Darius Mihai <dariusmihaim@gmail.com>

--- ramdisk/build_ramdisk.sh
+++ ramdisk/build_ramdisk.sh
@@ -1,15 +1,24 @@
-
-export ODIR=/root/obj/arm.armv6/root/bhyvearm/src
+export PLATFORM=${1:-CUBIEBOARD2}
+export ODIR=/root/freebsd-obj/arm.armv6/root/freebsd/
 
 cp ${ODIR}/sys/FVP_VE_CORTEX_A15x1_GUEST/kernel.bin .
 
 makefs -t ffs -B little\
 	-o optimization=space\
 	-o version=1\
-	ramdisk.img ramdisk.mtree
+	ramdisk-guest.img ramdisk-guest.mtree
+STATUS=$?
+if [ ${STATUS} != 0 ]; then
+    echo "Failed to build guest ramdisk"
+    exit ${STATUS}
+fi
 
 makefs -t ffs -B little\
 	-o optimization=space\
-	-o version=1\
-	ramdisk-guest.img ramdisk-guest.mtree
-
+	-o version=2\
+	ramdisk.img ramdisk.mtree
+STATUS=$?
+if [ ${STATUS} != 0 ]; then
+    echo "Failed to build host ramdisk"
+    exit ${STATUS}
+fi
